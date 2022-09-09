90efad0e2efffedb7bf5b89d25acef633f865cb6
Katrin K <katrin.kaik@tktk.ee>
2022-09-09 18:13:39 +0300

    create new app

---
+++ MyFirstApp1/MyFirstApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a commet to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" +", " + userName + "!"); //Output
+
+//strig interpolation
+Console.WriteLine($"Hello, {userName} !"); //Output
